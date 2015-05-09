6363e8e614b88906c5f2d162090233a4f052266a
MintPaw <Jerusanders@gmail.com>
2015-05-09 14:34:10 -0600

    Added key input

--- Source/game/Player.hx
+++ Source/game/Player.hx
@@ -1,5 +1,6 @@
 package game;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 
 class Player extends FlxSprite
@@ -10,5 +11,25 @@ class Player extends FlxSprite
 		super();
 
 		makeGraphic(30, 60, 0xFF0000FF);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		var left:Bool;
+		var right:Bool;
+		var up:Bool;
+		var down:Bool;
+		left = right = up = down = false;
+
+		{ // Update input
+			if (FlxG.keys.pressed.LEFT) left = true;
+			if (FlxG.keys.pressed.RIGHT) right = true;
+			if (FlxG.keys.pressed.UP) up = true;
+			if (FlxG.keys.pressed.DOWN) down = true;
+
+			trace(left, right, up, down);
+		}
+
+		super.update(elapsed);
 	}
 }
