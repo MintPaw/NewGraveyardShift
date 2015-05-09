1b70d3614abe378dc809d97fdd70792ee9612773
MintPaw <Jerusanders@gmail.com>
2015-05-09 14:04:58 -0600

    Added FlxGame

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,12 +1,17 @@
 package;
 
+import flixel.FlxGame;
 import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	private var _flixel:FlxGame;
 
 	public function new()
 	{
 		super();
+
+		_flixel = new FlxGame(1920, 1080, null, 120, 120, true, true);
+		addChild(_flixel);
 	}
 }
